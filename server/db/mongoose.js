9e2e07a05b8e5fa2e86a2debfb5948d2cd3c36c0
tan <kun82@hotmail.com>
2019-04-23 15:19:52 +0800

    Setup test & development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,10 @@ var mongoose = require ('mongoose')
 
 // set built-in promise
 mongoose.Promise=global.Promise
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp') //connecto TodoApp Database
+mongoose.connect(process.env.MONGODB_URI) 
 
 module.exports ={
     mongoose:mongoose
 }
 //https://intense-wildwood-22584.herokuapp.com/
+
