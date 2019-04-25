fe1eec8a05977bc92896a8dd5107bbf652551d8d
tan <kun82@hotmail.com>
2019-04-26 00:07:07 +0800

    Add test for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -298,3 +298,21 @@ describe('POST /users/login',()=>{
          })
     })
 })
+
+describe('DELETE /users/me/token',()=>{
+    it('should remove auth token on logout',(done)=>{
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token) //set header and access 1st user .token property
+        .expect(200)
+        .end((err,res)=>{ //custome callback to end
+            if(err){//if err calling done and return error
+                return done(err)
+            } //else find & access 1st users id 
+            User.findById(users[0]._id).then((user)=>{
+                expect(user.tokens.length).toBe(0) 
+                done()
+            }).catch((err)=> done(err))
+        })
+    })
+})
